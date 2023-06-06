0439afcd70d29c2d38dad2386871f3df37587a81
Saulo Regis <l4910@liga.org.br>
2023-06-06 11:07:46 -0300

    tela de login parte 1

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from './src/screens/cadastro';
+import LoginScreen from './src/screens/Login';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Cadastro />
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
