20f0797a4f9e100064ee88ab824fa96fc034ab95
Raone Victor da Cunha <raonevictor@gmail.com>
2023-06-21 23:37:42 -0300

    Adicionado listagem de sinais na tela Principal

--- src/screens/Principal.js
+++ src/screens/Principal.js
@@ -1,27 +1,98 @@
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, TouchableOpacity } from 'react-native'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+
+import { auth } from "../config/firebase";
+import { collection, getDocs, getFirestore, orderBy, query, where } from "firebase/firestore";
+
+const db = getFirestore(auth);
 
 const Principal = () => {
   const navigation = useNavigation();
+  const collec = collection(db, "/Sinais-Vitais");
+  const [sinais, setSinais] = useState([]);
 
-    return (
-      <SafeAreaView style={styles.container}>
-      <View styles={styles.container}>
-        <Text>Principal</Text>
-      </View>
-        <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('Cadastro Sinais Vitais')}>
-          <Ionicons name="add-outline" size={40} color="#1F2B5B" />
-        </TouchableOpacity>
-      </SafeAreaView>
+  useEffect(() => {
+    navigation.addListener("focus", async () => {
+      const q = query(collec, where("idUsuario", "==", "1q2w3e"), orderBy("dataCadastro", "desc"));
+      const docs = await getDocs(q);
+      const documents = [];
+
+      docs.forEach((doc) => {
+        documents.push({
+          ...doc.data(),
+          id: doc.id
+        });
+      });
+
+      setSinais(documents);
+    }
     )
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View styles={styles.container}>
+        <FlatList
+          data={sinais}
+          renderItem={({ item }) => (
+            <View style={[styles.signalsContainer, styles.width100]}>
+              <View>
+                <View style={[styles.signal, styles.width100]}>
+                  <Text style={styles.label}>Pressão (mmHg):</Text>
+                  <Text style={styles.value}>{item.pressao}</Text>
+                </View>
+
+                <View style={[styles.signal, styles.width100]}>
+                  <Text style={styles.label}>Glicose (mg/dL):</Text>
+                  <Text style={styles.value}>{item.glicose}</Text>
+                </View>
+
+                <View style={[styles.signal, styles.width100]}>
+                  <Text style={styles.label}>Temperatura (°C):</Text>
+                  <Text style={styles.value}>{item.temperatura}</Text>
+                </View>
+
+                <View style={[styles.signal, styles.width100]}>
+                  <Text style={styles.label}>Batimento (bpm):</Text>
+                  <Text style={styles.value}>{item.batimento}</Text>
+                </View>
+
+                <View style={[styles.signal, styles.width100]}>
+                  <Text style={styles.label}>Cadastrado em:</Text>
+                  <Text style={styles.value}>{item.dataCadastro}</Text>
+                </View>
+              </View>
+
+              <TouchableOpacity
+                style={[styles.button, styles.detButton]}
+                onPress={() => navigation.navigate('Detalhes Sinais Vitais', { idSinais: item.id })}
+              >
+                <Ionicons name="document-text-outline" size={40} color="#1F2B5B" />
+              </TouchableOpacity>
+            </View>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.button, styles.addButton]}
+        onPress={() => navigation.navigate('Cadastro Sinais Vitais')}
+      >
+        <Ionicons name="add-outline" size={40} color="#7ED957" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
 }
 
 export default Principal
 
 const styles = StyleSheet.create({
+  width100: {
+    width: "100%"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -29,13 +100,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  addButton: {
-    backgroundColor: '#7ED957',
+  signalsContainer: {
+    padding: 16,
+    backgroundColor: '#fafafa',
+    borderRadius: 8,
+    marginBottom: 16,
+    elevation: 2,
+    shadowColor: '#222',
+    flexDirection: "row",
+    alignItems: 'center'
+  },
+  signal: {
+    flexDirection: "row"
+  },
+  label: {
+    color: '#000',
+    fontWeight: "700",
+    width: '30%',
+    textAlign: 'right'
+  },
+  value: {
+    marginLeft: 8,
+    width: '70%'
+  },
+  button: {
     padding: 14,
     borderRadius: 100,
     position: 'absolute',
-    bottom: 24,
     right: 24,
-
+  },
+  detButton: {
+    backgroundColor: '#7ED957',
+    marginVertical: 'auto'
+  },
+  addButton: {
+    backgroundColor: '#1F2B5B',
+    bottom: 24,
   },
 })
